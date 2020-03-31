785a2ca0ae6aaf64c010c32ed33329826d05241a
Charder001 <rick.chardet@hva.nl>
2020-03-31 23:56:41 +0200

    added styling and functionality

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
  // Require third-party modules
-const express = require('express');
-const request = require('request');
-const bodyParser = require("body-parser")
-const fetch = require("node-fetch");
+ const express = require('express');
+ const request = require('request');
+ const bodyParser = require("body-parser")
+ const fetch = require("node-fetch");
+ const savedShirts = [];
+ const port = process.env.PORT || 3000
 
-// Config object
-// const config = {
-// 	port = process.env.PORT || 3000
-// }
+ // Create new express app in 'app'
+ const app = express();
+ // Link the templating engine to the express app
+ app.set('view engine', 'ejs');
+ // Tell the views engine/ejs where the template files are stored (Settingname, value)
+ app.set('views', 'views');
 
-const port = process.env.PORT || 3000
+ // Tell express to use a 'static' folder
+ app.use(express.static('public'));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({
+ 	extended: false
+ }))
 
-// Create new express app in 'app'
-const app = express();
-// Link the templating engine to the express app
-app.set('view engine', 'ejs');
-// Tell the views engine/ejs where the template files are stored (Settingname, value)
-app.set('views', 'views');
+ // Actually set up the server
+ app.listen(port, function () {
+ 	console.log(`Application started on port: ${port}`);
+ });
 
-// Tell express to use a 'static' folder
-// If the url matches a file it will send that file
-// Sending something (responding) ends the response cycle
-app.use(express.static('public'));
+ // Create a home route
+ app.get('/', function (req, res) {
+ 	res.render("home")
+ })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false })) 
-
-// Actually set up the server
-app.listen(port, function() {
-	console.log(`Application started on port: ${port}`);
-});
-
-// Create a home route
-app.get('/', function(req, res) {
-	res.render("home")
-  })
-
-app.post('/', function(req, res) {
-  console.log(req.body.shirtColor)
-	let shirtColor = req.body.shirtColor;
-	res.render("shirt", { color: shirtColor })
-});
+ app.post('/', function (req, res) {
+	 let uploadedShirtId = req.body.shirtId
+	 
+ 	if (uploadedShirtId !="" && uploadedShirtId !== undefined) {
+		 console.log("its true")
+		 console.log(uploadedShirtId)
+		 const uploadedShirt = savedShirts.filter(savedShirts => savedShirts.id === uploadedShirtId);
+		 console.log(uploadedShirt)
+		 res.render("shirt", {
+			id: uploadedShirt[0].id,
+			color: uploadedShirt[0].color,
+			textColor: uploadedShirt[0].textColor,
+			text: uploadedShirt[0].text
+		})
+ 	} else {
+ 		let shirtId = '_' + Math.random().toString(36).substr(2, 9);
+		 let shirtColor = req.body.shirtColor
+		 let textColor = req.body.textColor
+ 		let shirtText = req.body.shirtText
+ 		let saveThis = {
+ 			id: shirtId,
+			 color: shirtColor,
+			 textColor: textColor,
+ 			text: shirtText
+ 		}
+ 		savedShirts.push(saveThis)
+ 		console.log(savedShirts)
+ 		res.render("shirt", {
+ 			id: shirtId,
+			 color: shirtColor,
+			 textColor: textColor,
+ 			text: shirtText
+ 		})
+ 	}
+ });
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+document.getElementById("printButton").addEventListener("click", function(){
+    console.log("printing")
+    window.print()
+})
+
+function liveUpdate(){
+   var updatedText =  document.getElementById("textInput").value
+   document.getElementById("fillingText").innerText = updatedText
+}
