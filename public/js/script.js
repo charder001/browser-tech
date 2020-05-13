36bf6e7b3dbc5d0dce4bfd2091adafc49d5f2510
Charder001 <rick.chardet@hva.nl>
2020-05-13 21:39:54 +0200

    added live color changing

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+var main = document.querySelector("main")
+main.insertAdjacentHTML("beforeend", '<button value="Print" id="printButton">Print</button>')
 document.getElementById("printButton").addEventListener("click", function(){
     console.log("printing")
     window.print()
@@ -6,4 +8,21 @@ document.getElementById("printButton").addEventListener("click", function(){
 function liveUpdate(){
    var updatedText =  document.getElementById("textInput").value
    document.getElementById("fillingText").innerText = updatedText
+}
+
+const shirtSVG = document.getElementById('shirtSVG');
+let radioButtons = document.forms["shirt"].elements["shirtColor"];
+for (var i = 0, max = radioButtons.length; i < max; i++) {
+    radioButtons[i].onclick = function () {
+    shirtSVG.style.fill = this.value;
+  }
+}
+
+const shirtTextColor = document.getElementById('fillingText');
+let radioButtonsText = document.forms["shirt"].elements["textColor"];
+for (var i = 0, max = radioButtonsText.length; i < max; i++) {
+    radioButtonsText[i].onclick = function () {
+        shirtTextColor.style.color = this.value;
+        console.log(shirtTextColor.style.color)
+  }
 }
